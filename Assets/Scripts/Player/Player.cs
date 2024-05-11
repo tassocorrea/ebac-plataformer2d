3e8a107a52e51c600b444b699ac5d1c715fccc18
tassocorrea <168818323+tassocorrea@users.noreply.github.com>
2024-05-11 12:55:00 -0300

    implement first prototype of player walk

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public Rigidbody2D myrigidbody;
+
+    public Vector2 velocity;
+
+    public float speed;
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            //myrigidbody.MovePosition(myrigidbody.position - velocity * Time.deltaTime);
+            myrigidbody.velocity = new Vector2(-speed, myrigidbody.velocity.y);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            //myrigidbody.MovePosition(myrigidbody.position + velocity * Time.deltaTime);
+            myrigidbody.velocity = new Vector2(speed, myrigidbody.velocity.y);
+
+        }
+    }
+
+
+}
